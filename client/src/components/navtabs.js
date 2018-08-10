83924d2ab44bb32a7f5da406471b4477b8a189bb
Dylan-Desktop <dylanmthomas@live.com>
2018-08-10 14:38:36 -0600

    updated image carousel on home.js

--- client/src/components/navtabs.js
+++ client/src/components/navtabs.js
@@ -1,69 +1,78 @@
 import React from "react";
+import fbIcon from '../assets/images/if_fb_icon.png';
+import ytIcon from '../assets/images/if_yt_icon.png';
 
 const NavTabs = props => (
-  <ul className="nav nav-tabs">
-    <li className="nav-item">
-      <a
-        onClick={() => props.handlePageChange("Home")}
-        className={
-          props.currentPage === "Home" ? "nav-link active" : "nav-link"
-        }
-      >
-        Home
-      </a>
-    </li>
-    <li className="nav-item">
-      <a
-        onClick={() => props.handlePageChange("Superarm Gallery")}
-        className={
-          props.currentPage === "Superarm Gallery" ? "nav-link active" : "nav-link"
-        }
-      >
-        SA Gallery
-      </a>
-    </li>
-    <li className="nav-item">
-      <a
-        onClick={() => props.handlePageChange("Motorhome")}
-        className={
-          props.currentPage === "Motorhome" ? "nav-link active" : "nav-link"
-        }
-      >
-        SA 124 Motorhome Sprinter
-      </a>
-    </li>
-    <li className="nav-item">
-      <a
-        onClick={() => props.handlePageChange("History")}
-        className={
-          props.currentPage === "History" ? "nav-link active" : "nav-link"
-        }
-      >
-        History
-      </a>
-    </li>
-    <li className="nav-item">
-      <a
-        onClick={() => props.handlePageChange("Blog")}
-        className={
-          props.currentPage === "Blog" ? "nav-link active" : "nav-link"
-        }
-      >
-        Blog
-      </a>
-    </li>
-    <li className="nav-item">
-      <a
-        onClick={() => props.handlePageChange("Contact")}
-        className={
-          props.currentPage === "Contact" ? "nav-link active" : "nav-link"
-        }
-      >
-        Contact
-      </a>
-    </li>
-
-  </ul>
+    <div>
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <a
+                    onClick={() => props.handlePageChange("Home")}
+                    className={
+                        props.currentPage === "Home" ? "nav-link active" : "nav-link"
+                    }
+                >
+                    Home
+                </a>
+            </li>
+            <li className="nav-item">
+                <a
+                    onClick={() => props.handlePageChange("Superarm Gallery")}
+                    className={
+                        props.currentPage === "Superarm Gallery" ? "nav-link active" : "nav-link"
+                    }
+                >
+                    SA Gallery
+                </a>
+            </li>
+            <li className="nav-item">
+                <a
+                    onClick={() => props.handlePageChange("Motorhome")}
+                    className={
+                        props.currentPage === "Motorhome" ? "nav-link active" : "nav-link"
+                    }
+                >
+                    SA 124 Motorhome Sprinter
+                </a>
+            </li>
+            <li className="nav-item">
+                <a
+                    onClick={() => props.handlePageChange("History")}
+                    className={
+                        props.currentPage === "History" ? "nav-link active" : "nav-link"
+                    }
+                >
+                    History
+                </a>
+            </li>
+            <li className="nav-item">
+                <a
+                    onClick={() => props.handlePageChange("Blog")}
+                    className={
+                        props.currentPage === "Blog" ? "nav-link active" : "nav-link"
+                    }
+                >
+                    Blog
+                </a>
+            </li>
+            <li className="nav-item">
+                <a
+                    onClick={() => props.handlePageChange("Contact")}
+                    className={
+                        props.currentPage === "Contact" ? "nav-link active" : "nav-link"
+                    }
+                >
+                    Contact
+                </a>
+            </li>
+            <a href="https://www.facebook.com/Handicaps-Inc-SuperarmLiftcom-69384879635/" target="_blank" rel="noopener noreferrer">
+                <img id="fbIcon" src={fbIcon} alt="Facebook Link" />
+            </a>
+            <a href="https://www.youtube.com/channel/UCzb0X3OYp16FRuW4b32yZ7Q" target="_blank" rel="noopener noreferrer">
+                <img id="ytIcon" src={ytIcon} alt="Youtube Link" />
+            </a>
+        </ul>
+    </div>
 );
 
 export default NavTabs;
--- client/src/components/pages/home.js
+++ client/src/components/pages/home.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import bgBase from '../../assets/images/HC-Logo-Cube-bw-copy.jpg'
+import metris1 from '../../assets/images/Metris-2016-a.jpg';
+import metris2 from '../../assets/images/Metris-2016-b.jpg';
+import metris3 from '../../assets/images/Metris-2016-c.jpg';
+import {Carousel} from 'react-bootstrap';
 
 const Home = () => (
 
-<div>
+    <div>
+        <Carousel>
+            <Carousel.Item>
+                <img width={900} height={500} alt="900x500" src={metris1} />
+                <Carousel.Caption>
+                    <h3>2016 Mercedes Metris Superarm View from Outside</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img width={900} height={500} alt="900x500" src={metris2} />
+                <Carousel.Caption>
+                    <h3>2016 Mercedes Metris with wheelchair</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img width={900} height={500} alt="900x500" src={metris3} />
+                <Carousel.Caption>
+                    <h3>2016 Mercedes Metris Door Close-Up</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>;        <p className="App-intro">
+            Placeholder text about the company
+        </p>
 
-    <header className="App-header">
-
-        <img src={bgBase} className="App-logo" alt="logo" />
-
-        <h1 className="App-title">Welcome to Handicaps Web Page</h1>
-
-    </header>
-
-    <p className="App-intro">
-
-        Placeholder text about the company
-
-</p>
-
-</div>
+    </div>
 )
 
 export default Home;
